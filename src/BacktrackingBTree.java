c4bd38472b84408476c94395c5b53b5b3b5dd162
Itay Genkin <itay.genkin@gmail.com>
2021-05-16 13:50:42 +0300

    16/5/21 - starting to implement backtrack in BTree, adding some commands to insert in BTree

--- src/BacktrackingBTree.java
+++ src/BacktrackingBTree.java
@@ -1,16 +1,31 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class BacktrackingBTree<T extends Comparable<T>> extends BTree<T> {
 
 	//You are to implement the function Backtrack.
 	public void Backtrack() {
-	    IntegrityStatement.signature(); // Reminder!
-	    throw new UnsupportedOperationException("You should implement this");
+		if ( !added.isEmpty() ){
+			T value = added.removeFirst();
+			int number = numOfSplited.removeFirst();
+			T[] toBeMergeArray = (T[]) new Object[number];
+			for (int i=0; i<number; i++){
+				toBeMergeArray[i] = splited.removeFirst();
+			}
+			// remove(Value); //Todo: add a method remove to BacktrackBTree
+
+		}
     }
 	
 	//Change the list returned to a list of integers answering the requirements
 	public static List<Integer> BTreeBacktrackingCounterExample(){
-	    IntegrityStatement.signature(); // Reminder!
-	    throw new UnsupportedOperationException("You should implement this");
+		LinkedList<Integer> ans = new LinkedList<Integer>();
+		ans.addLast(3);
+		ans.addLast(4);
+		ans.addLast(6);
+		ans.addLast(8);
+		ans.addLast(9);
+		ans.addLast(7);
+		return ans;
 	}
 }
