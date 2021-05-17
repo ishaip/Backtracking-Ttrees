c6347b7ea7790ab335213af1790316fd1c902cf4
hitmy <ishai.picus@gmail.com>
2021-05-17 16:29:25 +0300

    -init-

--- src/BacktrackingBTree.java
+++ src/BacktrackingBTree.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class BacktrackingBTree<T extends Comparable<T>> extends BTree<T> {
@@ -5,12 +6,18 @@ public class BacktrackingBTree<T extends Comparable<T>> extends BTree<T> {
 	//You are to implement the function Backtrack.
 	public void Backtrack() {
 	    IntegrityStatement.signature(); // Reminder!
-	    throw new UnsupportedOperationException("You should implement this");
-    }
+		T[] arr = (T[]) new Object[5];
+	}
 	
 	//Change the list returned to a list of integers answering the requirements
 	public static List<Integer> BTreeBacktrackingCounterExample(){
-	    IntegrityStatement.signature(); // Reminder!
-	    throw new UnsupportedOperationException("You should implement this");
+		LinkedList<Integer> output = new LinkedList<Integer>();
+		output.addLast(3);
+		output.addLast(4);
+		output.addLast(6);
+		output.addLast(8);
+		output.addLast(9);
+		output.addLast(7);
+		return output;
 	}
 }
